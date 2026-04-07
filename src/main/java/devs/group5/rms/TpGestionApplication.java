2177664c760c7224c1022e56b81d895a8b6d77cb
Valentín Bersi <valentinbersi@gmail.com>
2026-04-07 02:29:05 +0000

    Created spring boot proyect with gradle

---
+++ src/main/java/devs/group5/rms/TpGestionApplication.java
@@ -0,0 +1,13 @@
+package devs.group5.rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGestionApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TpGestionApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/devs/group5/rms/TpGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.group5.rms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
