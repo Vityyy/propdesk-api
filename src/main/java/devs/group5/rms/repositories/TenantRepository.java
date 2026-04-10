1b77216082964f149f2a64f948bd91aa1a76df70
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:09:53 +0000

    feat: ✨ Add TenantRepository interface for Tenant entity with UUID support

---
+++ src/main/java/devs/group5/rms/repositories/TenantRepository.java
@@ -0,0 +1,9 @@
+package devs.group5.rms.repositories;
+
+import devs.group5.rms.entities.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TenantRepository extends JpaRepository<Tenant, UUID> {
+}
