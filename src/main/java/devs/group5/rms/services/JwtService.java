e710df3dfad0e5bdc2cf2d93a91e6116cecab147
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:22:10 +0000

    feat: ✨ Implement JwtService for JWT token generation

---
+++ src/main/java/devs/group5/rms/services/JwtService.java
@@ -0,0 +1,34 @@
+package devs.group5.rms.services;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+    private final JwtEncoder encoder;
+    private final int accessDuration;
+
+    public JwtService(JwtEncoder encoder, @Value("${jwt.accessDuration}") int accessDuration) {
+        this.encoder = encoder;
+        this.accessDuration = accessDuration;
+    }
+
+    public String generate(@NonNull UUID userId) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(userId.toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(accessDuration))
+                .build();
+
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
