a39f283864b1e713c9e0c7cfb6dab4f48c1c4c4b
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:08:40 +0000

    feat: ✨ Add AdminRepository interface for Admin entity with UUID support

---
+++ src/main/java/devs/group5/rms/repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package devs.group5.rms.repositories;
+
+import devs.group5.rms.entities.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
+}
