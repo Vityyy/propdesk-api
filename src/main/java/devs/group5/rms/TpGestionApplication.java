3c49e2fa1efbf07dbbe0af7860be8528fa45873c
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:13:01 +0000

    chore: 🧹 Remove unnecessary whitespace in TpGestionApplication.java

--- src/main/java/devs/group5/rms/TpGestionApplication.java
+++ src/main/java/devs/group5/rms/TpGestionApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TpGestionApplication {
-
     static void main(String[] args) {
         SpringApplication.run(TpGestionApplication.class, args);
     }
-    
 }
