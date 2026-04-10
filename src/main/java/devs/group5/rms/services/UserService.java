2519a85f52044c26569c9ceb7126b68f87782216
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:33:18 +0000

    feat: 🔍 Add UserService for user management operations
    Co-authored-by: Valentín Bersi <valentinbersi@gmail.com>

---
+++ src/main/java/devs/group5/rms/services/UserService.java
@@ -0,0 +1,19 @@
+package devs.group5.rms.services;
+
+import devs.group5.rms.entities.User;
+import devs.group5.rms.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User getUserById(UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
