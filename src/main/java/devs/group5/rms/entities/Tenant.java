30544d58f869ce042c2924cb5e17cc00e689770e
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:08:14 +0000

    feat: ✨ Add Tenant entity with UUID identifier and relationship to Apartment

---
+++ src/main/java/devs/group5/rms/entities/Tenant.java
@@ -0,0 +1,28 @@
+package devs.group5.rms.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "tenant")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE, onConstructor_ = @Builder)
+public class Tenant {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "apartment_id")
+    private Apartment apartment;
+}
