228d6c567d9620bb1a700c0ecf63a62d71d1ab80
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:21:55 +0000

    feat: ✨ Add JwtConfig for JWT encoding and decoding configuration

---
+++ src/main/java/devs/group5/rms/JwtConfig.java
@@ -0,0 +1,37 @@
+package devs.group5.rms;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import devs.group5.rms.properties.JwtProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+    private final String secret;
+
+    public JwtConfig(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey secretKey = new SecretKeySpec(secret.getBytes(), "${jwt.algorithm}");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        SecretKey key = new SecretKeySpec(secret.getBytes(), "${jwt.algorithm}");
+        return new NimbusJwtEncoder(new ImmutableSecret<>(key));
+    }
+}
