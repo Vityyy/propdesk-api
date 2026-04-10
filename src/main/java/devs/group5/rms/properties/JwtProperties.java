88e70883f45f48351d63125938d0fc4c9ca44572
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:21:40 +0000

    feat: ✨ Add JwtProperties configuration for JWT settings

---
+++ src/main/java/devs/group5/rms/properties/JwtProperties.java
@@ -0,0 +1,12 @@
+package devs.group5.rms.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+        Duration accessDuration,
+        String algorithm
+) {
+}
