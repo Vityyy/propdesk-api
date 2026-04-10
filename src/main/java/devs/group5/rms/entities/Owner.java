6c8d97cc945db3ac94ae827fb3caea6fb7fac17c
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-10 02:06:51 +0000

    feat: ✨ Add Owner entity with relationships to Admin and Property

---
+++ src/main/java/devs/group5/rms/entities/Owner.java
@@ -0,0 +1,42 @@
+package devs.group5.rms.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "owner")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Owner extends User {
+    @JoinColumn(name = "admin_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Admin admin;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Property> properties;
+
+    @Builder
+    public Owner(UUID id, String name, Admin admin, List<Property> properties) {
+        super(id, name);
+        this.admin = admin;
+        this.properties = properties;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Owner owner = (Owner) o;
+        return super.equals(owner);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
