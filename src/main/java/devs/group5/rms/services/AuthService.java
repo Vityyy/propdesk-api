746f03239c18b79b2b74696808f4de4198612e6a
Valentín Bersi <valentinbersi@gmail.com>
2026-04-11 22:36:33 +0000

    feat (sign up): ✨ added sign up service methods for creating admins and owners

--- src/main/java/devs/group5/rms/services/AuthService.java
+++ src/main/java/devs/group5/rms/services/AuthService.java
@@ -1,5 +1,9 @@
 package devs.group5.rms.services;
 
+import devs.group5.rms.entities.Admin;
+import devs.group5.rms.entities.Owner;
+import devs.group5.rms.repositories.AdminRepository;
+import devs.group5.rms.repositories.OwnerRepository;
 import devs.group5.rms.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -13,10 +17,26 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private final AdminRepository adminRepository;
+    private final OwnerRepository ownerRepository;
 
     @Transactional
     public String login(String name) {
         val user = userRepository.findByName(name).orElseThrow(() -> new BadCredentialsException("User does not exist"));
         return jwtService.generate(user.getId());
+    }
+
+    @Transactional
+    public Admin signInAdmin(String name) {
+        var admin = Admin.builder().name(name).build();
+        admin = adminRepository.save(admin);
+        return admin;
+    }
+
+    @Transactional
+    public Owner signInOwner(String name) {
+        var owner = Owner.builder().name(name).build();
+        owner = ownerRepository.save(owner);
+        return owner;
     }
 }
