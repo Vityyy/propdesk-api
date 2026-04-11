1c12cd5d41650307660682250939e5a34d99bf72
Valentín Bersi <valentinbersi@gmail.com>
2026-04-11 22:08:07 +0000

    feat (Login): ✨ added findByName method to UserRepository

--- src/main/java/devs/group5/rms/repositories/UserRepository.java
+++ src/main/java/devs/group5/rms/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package devs.group5.rms.repositories;
 import devs.group5.rms.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByName(String name);
 }
