3b01d58dcfa440c53c48de3cc54fe9bc3c4f503a
Valentín Bersi <valentinbersi@gmail.com>
2026-04-11 21:54:47 +0000

    feat (Authentication): ✨ added auth service

---
+++ src/main/java/devs/group5/rms/services/AuthService.java
@@ -0,0 +1,22 @@
+package devs.group5.rms.services;
+
+import devs.group5.rms.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor(onConstructor_ = @Autowired)
+class AuthService {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Transactional
+    String login(String name) {
+        val user = userRepository.findByName(name).orElseThrow(() -> new BadCredentialsException("User does not exist"));
+        return jwtService.generate(user.getId());
+    }
+}
