7375feff63907cc70b2e7359c61182bd7eec3caf
Valentín Bersi <valentinbersi@gmail.com>
2026-04-11 21:35:13 +0000

    feat (expenses): ✨ added Expense entity

---
+++ src/main/java/devs/group5/rms/entities/Expense.java
@@ -0,0 +1,51 @@
+package devs.group5.rms.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE, onConstructor_ = @Builder)
+@Table(name = "expenses", uniqueConstraints = @UniqueConstraint(columnNames = {"description", "property_id"}))
+public class Expense {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false, name = "category")
+    private String category;
+    
+    @NotBlank
+    @Column(nullable = false, name = "category")
+    private String description;
+
+    @NotNull
+    @Positive
+    @Column(nullable = false, name = "amount")
+    private BigDecimal amount;
+
+    @NotNull
+    @Column(nullable = false, name = "date")
+    private BigDecimal date;
+
+    @NotNull
+    @Column(nullable = false, name = "payment_status")
+    private PaymentStatus paymentStatus;
+
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "property_id", nullable = false)
+    private Property property;
+}
