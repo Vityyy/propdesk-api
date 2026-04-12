9d8c6038f7da0a0c11a85ac48d0575402e384d8f
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-12 23:56:35 +0000

    refactor: ♻️ reorganize annotations and formatting in Property entity

--- src/main/java/devs/group5/rms/entities/Property.java
+++ src/main/java/devs/group5/rms/entities/Property.java
@@ -11,34 +11,31 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @ToString
+@Builder
+@NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@AllArgsConstructor(access = AccessLevel.PRIVATE, onConstructor_ = @Builder)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "properties", uniqueConstraints = @UniqueConstraint(columnNames = {"name", "address", "owner_id"}))
 public class Property {
+
     @Id
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
     @NotBlank
     @Column(nullable = false, name = "name")
     private String name;
-
     @NotBlank
     @Column(nullable = false, name = "address")
     private String address;
-
     @NotNull
     @ToString.Exclude
     @JoinColumn(name = "owner_id")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private Owner owner;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "property", orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Apartment> apartments;
-    
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "property", orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Expense> expenses;
 }
