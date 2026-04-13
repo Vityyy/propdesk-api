910a753ded54f0b9f84def5ccb2482d079f7af59
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-13 00:32:36 +0000

    feat: ✨ add PropertyController for handling property creation requests

---
+++ src/main/java/devs/group5/rms/controllers/PropertyController.java
@@ -0,0 +1,33 @@
+package devs.group5.rms.controllers;
+
+import devs.group5.rms.data.PropertyData;
+import devs.group5.rms.dtos.PropertyRequest;
+import devs.group5.rms.dtos.PropertyResponse;
+import devs.group5.rms.services.OwnerService;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/property")
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class PropertyController {
+    private final OwnerService ownerService;
+
+    @PostMapping
+    public PropertyResponse addProperty(@RequestBody PropertyRequest request) {
+        val property = new PropertyData(request.name(), request.address(), request.ownerId());
+        val response = ownerService.addProperty(property);
+
+        return new PropertyResponse(
+                response.getId(),
+                response.getName(),
+                response.getAddress(),
+                response.getOwner().getId()
+        );
+    }
+}
