44d2574df823ddb6682fbd8f879d74081b5a25f7
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-13 00:51:51 +0000

    feat: 🔍 add ApartmentController for apartment management and create ExpenseResponse record

---
+++ src/main/java/devs/group5/rms/controllers/ApartmentController.java
@@ -0,0 +1,36 @@
+package devs.group5.rms.controllers;
+
+import devs.group5.rms.data.ApartmentData;
+import devs.group5.rms.dtos.ApartmentRequest;
+import devs.group5.rms.dtos.ApartmentResponse;
+import devs.group5.rms.services.OwnerService;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/apartment")
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class ApartmentController {
+    private final OwnerService ownerService;
+
+    @PostMapping
+    public ApartmentResponse addApartment(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestBody ApartmentRequest request
+    ) {
+        val apartment = ownerService.addApartment(
+                UUID.fromString(jwt.getId()),
+                new ApartmentData(request.name(), request.propertyId())
+        );
+        return new ApartmentResponse(apartment.getId(), apartment.getName(), apartment.getProperty().getId());
+    }
+}
