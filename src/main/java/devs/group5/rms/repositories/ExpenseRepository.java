a4c9b232ac83d3ae99963d5d87c7d5715ba8cc0a
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-13 01:06:40 +0000

    feat: 🔍 add ExpenseRepository interface for expense data management

---
+++ src/main/java/devs/group5/rms/repositories/ExpenseRepository.java
@@ -0,0 +1,11 @@
+package devs.group5.rms.repositories;
+
+import devs.group5.rms.entities.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+}
