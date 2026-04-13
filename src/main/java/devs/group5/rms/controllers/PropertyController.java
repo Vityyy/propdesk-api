9e84945c506d53264e6052405ea0f000c55b94df
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-13 01:03:04 +0000

    feat: 🔒 update addProperty method in PropertyController to include JWT authentication for property owner validation

--- src/main/java/devs/group5/rms/controllers/PropertyController.java
+++ src/main/java/devs/group5/rms/controllers/PropertyController.java
@@ -7,10 +7,14 @@ import devs.group5.rms.services.OwnerService;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/property")
@@ -19,9 +23,16 @@ public class PropertyController {
     private final OwnerService ownerService;
 
     @PostMapping
-    public PropertyResponse addProperty(@RequestBody PropertyRequest request) {
+    public PropertyResponse addProperty(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestBody PropertyRequest request
+    ) {
         val property = new PropertyData(request.name(), request.address(), request.ownerId());
-        val response = ownerService.addProperty(property);
+        val response = ownerService.addProperty(
+                UUID.fromString(jwt.getId()),
+                property
+        );
+
 
         return new PropertyResponse(
                 response.getId(),
