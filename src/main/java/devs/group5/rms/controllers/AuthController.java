e3cf7bd00ef1bca2d16f158eac06c74446da52c4
Valentín Bersi <valentinbersi@gmail.com>
2026-04-14 22:06:52 +0000

    feat (US-12 - Login): ✨ added /auth/login, /auth/refresh and /auth/logout endpoints to AuthController

--- src/main/java/devs/group5/rms/controllers/AuthController.java
+++ src/main/java/devs/group5/rms/controllers/AuthController.java
@@ -5,25 +5,56 @@ import devs.group5.rms.dtos.SignUpRequest;
 import devs.group5.rms.dtos.TokenResponse;
 import devs.group5.rms.dtos.UserResponse;
 import devs.group5.rms.services.AuthService;
+import devs.group5.rms.services.CookieService;
+import devs.group5.rms.services.JwtService;
+import devs.group5.rms.services.UserService;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class AuthController {
+    private final JwtService jwtService;
     private final AuthService authService;
+    private final UserService userService;
+    private final CookieService cookieService;
 
     @PostMapping("/login")
-    public TokenResponse login(@RequestBody LoginRequest request) {
-        val name = request.name();
-        val token = authService.login(name);
-        return new TokenResponse(token);
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
+        val user = userService.getUserByName(request.name());
+
+        val accessToken = jwtService.generateAccessToken(user.getId(), user.getRole());
+        val refreshToken = jwtService.generateRefreshToken(user.getId(), user.getRole());
+
+        val cookie = cookieService.createRefreshTokenCookie(refreshToken);
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new TokenResponse(accessToken));
+    }
+
+    @PostMapping("/refresh")
+    public TokenResponse refresh(
+            @CookieValue(name = "refreshToken") String refreshToken
+    ) {
+        val accessToken = authService.refresh(refreshToken);
+        return new TokenResponse(accessToken);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        val cookie = cookieService.deleteRefreshTokenCookie();
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 
     @PostMapping("/register/admin")
