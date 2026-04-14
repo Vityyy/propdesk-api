7853e0f44d53c95c1e349e97510e7e9f2929f214
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-14 20:07:25 +0000

    feat 🔄 update request mappings in ApartmentController and PropertyController to use plural forms

--- src/main/java/devs/group5/rms/controllers/ApartmentController.java
+++ src/main/java/devs/group5/rms/controllers/ApartmentController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/apartment")
+@RequestMapping("/apartments")
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class ApartmentController {
     private final OwnerService ownerService;
