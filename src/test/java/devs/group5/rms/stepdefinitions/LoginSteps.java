96ceebd34504f2c6459f1de80a701265921052b0
Valentín Bersi <valentinbersi@gmail.com>
2026-04-18 01:44:25 +0000

    test: 🚦 removed config annotations from LoginSteps

--- src/test/java/devs/group5/rms/stepdefinitions/LoginSteps.java
+++ src/test/java/devs/group5/rms/stepdefinitions/LoginSteps.java
@@ -11,18 +11,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.spring.CucumberContextConfiguration;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test")
 public class LoginSteps {
 
     @Autowired
