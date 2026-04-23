510fa12eb0a06339479eb81ddc822b3fed2989a0
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-23 02:04:13 +0000

    refactor: 🔨 simplify imports in ApartmentController

--- src/main/java/devs/group5/rms/controllers/ApartmentController.java
+++ src/main/java/devs/group5/rms/controllers/ApartmentController.java
@@ -9,14 +9,10 @@ import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -52,5 +48,3 @@ public class ApartmentController {
                 .collect(Collectors.toList());
     }
 }
-
-
