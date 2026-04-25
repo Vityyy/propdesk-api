8cd53c4c7008af09e5607d2dd0c297c7780840e1
Valentín Bersi <valentinbersi@gmail.com>
2026-04-25 03:44:10 +0000

    feat (GDSI-29 - Apartment classification): ✨ updated /{propertyId}/apartments endpoint in PropertyController to return the apartments grouped by floor and number

--- src/main/java/devs/group5/rms/controllers/PropertyController.java
+++ src/main/java/devs/group5/rms/controllers/PropertyController.java
@@ -1,18 +1,27 @@
 package devs.group5.rms.controllers;
 
 import devs.group5.rms.data.PropertyData;
-import devs.group5.rms.dtos.PropertyApartmentsResponse;
+import devs.group5.rms.dtos.ApartmentWithTenantResponse;
 import devs.group5.rms.dtos.PropertyRequest;
 import devs.group5.rms.dtos.PropertyResponse;
+import devs.group5.rms.dtos.TenantResponse;
+import devs.group5.rms.services.ApartmentService;
 import devs.group5.rms.services.OwnerService;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -21,6 +30,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class PropertyController {
     private final OwnerService ownerService;
+    private final ApartmentService apartmentService;
 
     @PostMapping
     public PropertyResponse addProperty(
@@ -66,11 +76,34 @@ public class PropertyController {
     }
 
     @GetMapping("/{propertyId}/apartments")
-    public List<PropertyApartmentsResponse> getPropertyApartments(
+    public Map<Integer, Map<Integer, ApartmentWithTenantResponse>> getPropertyApartments(
             @AuthenticationPrincipal Jwt jwt,
             @PathVariable UUID propertyId
     ) {
-        val ownerId = UUID.fromString(jwt.getSubject());
-        return ownerService.getPropertyApartmentsGrid(ownerId, propertyId);
+        val apartmentsByFloor = apartmentService.getApartmentsFromPropertyByFloor(propertyId);
+
+        return apartmentsByFloor.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry
+                                .getValue()
+                                .entrySet()
+                                .stream()
+                                .collect(Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        e -> new ApartmentWithTenantResponse(
+                                                e.getValue().id(),
+                                                e.getValue().dueDate(),
+                                                e.getValue().paymentStatus(),
+                                                e.getValue().squareMeters(),
+                                                e.getValue().rent(),
+                                                new TenantResponse(
+                                                        e.getValue().tenant().id(),
+                                                        e.getValue().tenant().name()
+                                                )
+                                        )
+                                ))
+                ));
     }
 }
