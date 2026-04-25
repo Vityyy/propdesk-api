00df0e4c7b8b96108895100a374b537e3f6d2836
Valentín Bersi <valentinbersi@gmail.com>
2026-04-25 03:42:26 +0000

    feat (GSDI-29 - Apartment classification): ✨ added ApartmentWithTenantData record

---
+++ src/main/java/devs/group5/rms/data/ApartmentWithTenantData.java
@@ -0,0 +1,19 @@
+package devs.group5.rms.data;
+
+import devs.group5.rms.entities.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ApartmentWithTenantData(
+        UUID id,
+        int number,
+        LocalDate dueDate,
+        PaymentStatus paymentStatus,
+        int floor,
+        BigDecimal squareMeters,
+        BigDecimal rent,
+        TenantData tenant
+) {
+}
