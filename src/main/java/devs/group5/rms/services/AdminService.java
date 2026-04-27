79454630ef23d379502825a4a62dd03839cf56ec
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-04-27 03:03:59 +0000

    feat: ✨ remove expense management functionality from AdminService

--- src/main/java/devs/group5/rms/services/AdminService.java
+++ src/main/java/devs/group5/rms/services/AdminService.java
@@ -17,26 +17,4 @@ public class AdminService {
     public List<Admin> getAdmins() {
         return adminRepository.findAll();
     }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @Transactional
-    public Expense addExpense(@NonNull UUID authenticatedUserId, @NonNull ExpenseData data) {
-        val property = propertyRepository.findById(data.propertyId()).orElseThrow(() -> new RuntimeException("Property does not exist"));
-
-        val owner = property.getOwner();
-        if (owner.getAdmin() == null || !owner.getAdmin().getId().equals(authenticatedUserId)) {
-            throw new RuntimeException("Property does not belong to an owner associated with authenticated admin");
-        }
-
-        val expense = Expense.builder()
-                .category(data.category())
-                .description(data.description())
-                .amount(data.amount())
-                .date(data.date())
-                .paymentStatus(PaymentStatus.PENDING)
-                .property(property)
-                .build();
-
-        return expenseRepository.save(expense);
-    }
 }
