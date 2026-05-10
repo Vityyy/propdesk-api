d7fd5582dd8a1937487e9cd1aee1745d3d370318
Camilo Sassone Irrazabal <camilosassone@outlook.com>
2026-05-10 22:54:36 +0000

    feat: ✨ add property existence check and handle deletion of related tenants in PropertyService

--- src/main/java/devs/group5/rms/repositories/PropertyRepository.java
+++ src/main/java/devs/group5/rms/repositories/PropertyRepository.java
@@ -4,9 +4,11 @@ import devs.group5.rms.entities.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PropertyRepository extends JpaRepository<Property, UUID> {
     List<Property> findByOwner_Id(UUID ownerId);
     List<Property> findByOwner_IdAndIsDeletedFalse(UUID ownerId);
+    Optional<Property> findByOwner_IdAndNameAndAddress(UUID ownerId, String name, String address);
 }
